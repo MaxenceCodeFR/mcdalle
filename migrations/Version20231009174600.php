db07457140822ca0145f654f4e9311100bd5a5ae
MaxenceCodeFR <maxence.hattabi@gmail.com>
2023-10-09 20:07:09 +0200

    catégories en bdd

---
+++ migrations/Version20231009174600.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231009174600 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE categories (id INT AUTO_INCREMENT NOT NULL, parent_id_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, category_order INT NOT NULL, INDEX IDX_3AF34668B3750AF4 (parent_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE categories ADD CONSTRAINT FK_3AF34668B3750AF4 FOREIGN KEY (parent_id_id) REFERENCES categories (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE categories DROP FOREIGN KEY FK_3AF34668B3750AF4');
+        $this->addSql('DROP TABLE categories');
+    }
+}
---
+++ src/Entity/Categories.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoriesRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoriesRepository::class)]
+class Categories
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $category_order = null;
+
+    #[ORM\ManyToOne(targetEntity: self::class, inversedBy: 'categories')]
+    private ?self $parent_id = null;
+
+    #[ORM\OneToMany(mappedBy: 'parent_id', targetEntity: self::class)]
+    private Collection $categories;
+
+    public function __construct()
+    {
+        $this->categories = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCategoryOrder(): ?int
+    {
+        return $this->category_order;
+    }
+
+    public function setCategoryOrder(int $category_order): static
+    {
+        $this->category_order = $category_order;
+
+        return $this;
+    }
+
+    public function getParentId(): ?self
+    {
+        return $this->parent_id;
+    }
+
+    public function setParentId(?self $parent_id): static
+    {
+        $this->parent_id = $parent_id;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, self>
+     */
+    public function getCategories(): Collection
+    {
+        return $this->categories;
+    }
+
+    public function addCategory(self $category): static
+    {
+        if (!$this->categories->contains($category)) {
+            $this->categories->add($category);
+            $category->setParentId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCategory(self $category): static
+    {
+        if ($this->categories->removeElement($category)) {
+            // set the owning side to null (unless already changed)
+            if ($category->getParentId() === $this) {
+                $category->setParentId(null);
+            }
+        }
+
+        return $this;
+    }
+}
