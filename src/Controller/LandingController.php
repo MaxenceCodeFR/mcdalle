bade5f4b54f9a899d651da6f02494186cfc38b27
MaxenceCodeFR <maxence.hattabi@gmail.com>
2023-10-11 14:34:40 +0200

    bouton de retour + selection des burgers ok

--- src/Controller/LandingController.php
+++ src/Controller/LandingController.php
@@ -23,4 +23,24 @@ class LandingController extends AbstractController
 
         return $this->render('landing/index.html.twig', compact('categories', 'subcategories'));
     }
+
+    #[Route('/choose/{id}', name: 'subcategory')]
+    public function choose(Categories $categories): Response
+    {
+
+        $products = $categories->getProducts();
+
+        return $this->render('landing/choose.html.twig', compact('products', 'categories'));
+    }
+
+    #[Route('/{id}', name: 'details')]
+    public function details(Products $products): Response
+    {
+
+
+
+        $allergens = $products->getAllergens();
+
+        return $this->render('landing/details.html.twig', compact('products', 'allergens'));
+    }
 }
