2990136ce79367df28eb56c167b94d128139cd54
MaxenceCodeFR <maxence.hattabi@gmail.com>
2023-10-13 19:47:36 +0200

    RAS

--- src/Controller/Admin/ProductsController.php
+++ src/Controller/Admin/ProductsController.php
@@ -4,14 +4,15 @@ namespace App\Controller\Admin;
 
 
 use App\Entity\Products;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\ProductsFormType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-#[Route('/admin', name: 'admin_')]
+#[Route('/admin/product', name: 'admin_')]
 class ProductsController extends AbstractController
 {
-    #[Route('/product', name: 'index')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('admin/index.html.twig');
@@ -20,13 +21,21 @@ class ProductsController extends AbstractController
     #[Route('/add', name: 'add')]
     public function add(): Response
     {
+        //Route accessible uniquement par le/les admins sauf PRODUCT_ADMIN cf. security.yaml et Voter
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
-        return $this->render('admin/index.html.twig');
+
+        $product = new Products();
+        $productForm = $this->createForm(ProductsFormType::class, $product);
+
+        return $this->render('admin/add.html.twig', [
+            'productForm' => $productForm->createView()
+        ]);
     }
 
     #[Route('/edit/{id}', name: 'edit')]
     public function edit(Products $products): Response
     {
+        //Route accessible uniquement par l'admin "PRODUCT_ADMIN" cf. security.yaml et Voter
         $this->denyAccessUnlessGranted('edit', $products);
         return $this->render('admin/index.html.twig');
     }
@@ -34,6 +43,7 @@ class ProductsController extends AbstractController
     #[Route('/delete/{id}', name: 'delete')]
     public function delete(Products $products): Response
     {
+        //Route accessible uniquement par l'admin "SUPER_ADMIN" et "ADMIN" cf. security.yaml et Voter
         $this->denyAccessUnlessGranted('delete', $products);
         return $this->render('admin/index.html.twig');
     }
