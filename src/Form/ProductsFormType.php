8fb8253ab48c83a8ceee55c20d799f88186b3384
MaxenceCodeFR <maxence.hattabi@gmail.com>
2023-10-13 20:07:33 +0200

    on peut maintenant selectionner plusieurs allergènes ainsi que une catégorie

--- src/Form/ProductsFormType.php
+++ src/Form/ProductsFormType.php
@@ -2,9 +2,13 @@
 
 namespace App\Form;
 
+use App\Entity\Allergens;
 use App\Entity\Products;
+use App\Entity\Categories;
+use Doctrine\ORM\Mapping\Entity;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProductsFormType extends AbstractType
@@ -12,11 +16,26 @@ class ProductsFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', options: [
+                'label' => 'Nom du produit'
+            ])
             ->add('images')
-            ->add('price');
-        // ->add('categories_id')
-        // ->add('allergens');
+            ->add('price', options: [
+                'label' => 'Prix du produit'
+            ])
+            ->add('categories_id', EntityType::class, options: [
+                'class' => Categories::class,
+                'choice_label' => 'name',
+                'group_by' => 'parent_id.name',
+
+            ])
+            ->add('allergens', EntityType::class, options: [
+                'class' => Allergens::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'label' => 'Allergènes'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
