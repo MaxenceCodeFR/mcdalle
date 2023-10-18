6201fa8a2c25a9d6a17acf2a59fed1827b929585
MaxenceCodeFR <maxence.hattabi@gmail.com>
2023-10-18 12:00:26 +0200

    admin_interface carrément ok

--- src/Controller/Admin/ProductsController.php
+++ src/Controller/Admin/ProductsController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-#[Route('/admin/product', name: 'admin_')]
+#[Route('/admin-interface/product', name: 'admin_')]
 class ProductsController extends AbstractController
 {
     #[Route('/', name: 'index')]
